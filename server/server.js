1eb0fde400fbf16f6c3f49b5563cfc189b281611
jigyasasinhal4-boop <jigyasasinhal4@gmail.com>
2026-07-24 12:31:33 +0530

    Update server.js
    Connect frontend to Render backend

--- server/server.js
+++ server/server.js
@@ -2,13 +2,14 @@ require("dotenv").config();
 console.log(process.env.RESEND_API_KEY);
 const express = require("express");
 const cors = require("cors");
+const hpp = require("hpp");
 const paymentRoutes = require("./routes/payment");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/payment", paymentRoutes);
-
+app.use(hpp());
 app.get("/", (req, res) => {
     res.send("The Hills Still Call Backend Running ❤️");
 });
